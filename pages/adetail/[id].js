e6e2ad20ea5d426c610c109ade32f7d0b567711c
jeodiong <jeodiong@hotmail.com>
2020-08-21 11:53:36 +0800

    feat:  initial commit

--- pages/adetail/[id].js
+++ pages/adetail/[id].js
@@ -1,53 +1,74 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import AV from 'leancloud-storage'
-import dynamic from 'next/dynamic'
-import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import marked from 'marked'
-import hljs from 'highlight.js'
-import { BackTop, message } from 'antd'
-import dayjs from 'dayjs'
-import ReactScroll from 'react-scroll'
+import Head from "next/head";
+import Link from "next/link";
+import AV from "leancloud-storage";
+import dynamic from "next/dynamic";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import marked from "marked";
+import hljs from "highlight.js";
+import { BackTop, message } from "antd";
+import dayjs from "dayjs";
+import ReactScroll from "react-scroll";
 
-import styles from './index.module.scss'
-import Layout from 'src/components/www/Layout'
-import Comments from 'src/components/www/Comments'
-import CategoryList from 'src/components/www/CategoryList'
-import Ad from 'src/components/www/Ad'
-import UserInfo from 'src/components/www/UserInfo'
-import { getArticleById, updateArticle } from 'src/service/article'
-import { createCollect, deleteCollect, countCollect, getMyCollect } from 'src/service/collect'
-import { createLike, deleteLike, countLike, getMyLike } from 'src/service/likes'
-import { countComments } from 'src/service/comment'
+import styles from "./index.module.scss";
+import Layout from "src/components/www/Layout";
+import Comments from "src/components/www/Comments";
+import CategoryList from "src/components/www/CategoryList";
+import Ad from "src/components/www/Ad";
+import UserInfo from "src/components/www/UserInfo";
+import { getArticleById, updateArticle } from "src/service/article";
+import {
+  createCollect,
+  deleteCollect,
+  countCollect,
+  getMyCollect,
+} from "src/service/collect";
+import {
+  createLike,
+  deleteLike,
+  countLike,
+  getMyLike,
+} from "src/service/likes";
+import { countComments } from "src/service/comment";
 
-require('dayjs/locale/zh-cn')
-dayjs.locale('zh-cn')
-const relativeTime = require('dayjs/plugin/relativeTime')
-dayjs.extend(relativeTime)
+require("dayjs/locale/zh-cn");
+dayjs.locale("zh-cn");
+const relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
 
-const nav = []
-const renderer = new marked.Renderer()
+const nav = [];
+const renderer = new marked.Renderer();
 renderer.heading = function heading(text, level, raw, slugger) {
   nav.push({
     text,
     level,
     raw,
     slugger,
-    id: this.options.headerPrefix + slugger.slug(raw) + '-1',
-  })
+    id: this.options.headerPrefix + slugger.slug(raw) + "-1",
+  });
   if (this.options.headerIds) {
-    return '<h' + level + ' id="' + this.options.headerPrefix + slugger.slug(raw) + '">' + text + '</h' + level + '>\n'
+    return (
+      "<h" +
+      level +
+      ' id="' +
+      this.options.headerPrefix +
+      slugger.slug(raw) +
+      '">' +
+      text +
+      "</h" +
+      level +
+      ">\n"
+    );
   } // ignore IDs
-  return '<h' + level + '>' + text + '</h' + level + '>\n'
-}
+  return "<h" + level + ">" + text + "</h" + level + ">\n";
+};
 
 marked.setOptions({
   renderer,
   highlight: function (code, language) {
-    const hljs = require('highlight.js')
-    const validLanguage = hljs.getLanguage(language) ? language : 'plaintext'
-    return hljs.highlight(validLanguage, code).value
+    const hljs = require("highlight.js");
+    const validLanguage = hljs.getLanguage(language) ? language : "plaintext";
+    return hljs.highlight(validLanguage, code).value;
   },
   pedantic: false,
   gfm: true,
@@ -56,63 +77,63 @@ marked.setOptions({
   smartLists: true,
   smartypants: false,
   xhtml: false,
-})
+});
 function MyComponent() {
-  const router = useRouter()
-  const aid = router.query.id
-  const [articleObj, setarticleObj] = useState(null)
-  const [articleObjJSON, setarticleObjJSON] = useState(null)
-  const [author, setauthor] = useState(null)
-  const [profile, setprofile] = useState(null)
-  const [userinfo, setuserinfo] = useState(null)
-  const [html, sethtml] = useState(null)
-  const [views, setviews] = useState(0)
-  const [num_collects, setnum_collects] = useState(0)
-  const [myCollect, setmyCollect] = useState(null)
+  const router = useRouter();
+  const aid = router.query.id;
+  const [articleObj, setarticleObj] = useState(null);
+  const [articleObjJSON, setarticleObjJSON] = useState(null);
+  const [author, setauthor] = useState(null);
+  const [profile, setprofile] = useState(null);
+  const [userinfo, setuserinfo] = useState(null);
+  const [html, sethtml] = useState(null);
+  const [views, setviews] = useState(0);
+  const [num_collects, setnum_collects] = useState(0);
+  const [myCollect, setmyCollect] = useState(null);
 
-  const [num_comments, setnum_comments] = useState(0)
+  const [num_comments, setnum_comments] = useState(0);
 
-  const [num_likes, setnum_likes] = useState(0)
-  const [myLike, setmyLike] = useState(null)
+  const [num_likes, setnum_likes] = useState(0);
+  const [myLike, setmyLike] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       if (aid) {
-        const res = await getArticleById({ id: aid })
-        setarticleObj(res)
-        setarticleObjJSON(res.toJSON())
-        setauthor(res.attributes.author)
-        sethtml(marked(res.attributes.articleVal))
+        const res = await getArticleById({ id: aid });
+        setarticleObj(res);
+        setarticleObjJSON(res.toJSON());
+        setauthor(res.attributes.author);
+        sethtml(marked(res.attributes.articleVal));
         // 访问数+1
         await updateArticle({
           articleItem: res,
           params: { views: 1 },
-        })
-        setviews(res.attributes.views)
+        });
+        setviews(res.attributes.views);
         // 收藏数量
-        setnum_collects(await countCollect({ article: res }))
+        setnum_collects(await countCollect({ article: res }));
         // 我是否收藏
-        const numCollect = await getMyCollect({ article: res })
-        setmyCollect(numCollect)
+        const numCollect = await getMyCollect({ article: res });
+        setmyCollect(numCollect);
 
         // 收藏数量
-        setnum_likes(await countLike({ article: res }))
+        setnum_likes(await countLike({ article: res }));
         // 我是否收藏
-        const numLike = await getMyLike({ article: res })
-        setmyLike(numLike)
+        const numLike = await getMyLike({ article: res });
+        setmyLike(numLike);
 
         // 评论数量
-        const numComments = await countComments({ article: res })
-        setnum_comments(numComments)
+        const numComments = await countComments({ article: res });
+        setnum_comments(numComments);
       }
     }
-    fetchData()
-  }, [router])
+    fetchData();
+  }, [router]);
 
   const handleCollect = async () => {
     await handleAction({
-      actionName: '收藏',
-      actionName_en: 'collects',
+      actionName: "收藏",
+      actionName_en: "collects",
       myItem: myCollect,
       createFunc: createCollect,
       delectMyItemFunc: deleteCollect,
@@ -121,13 +142,13 @@ function MyComponent() {
       setMyItemFunc: setmyCollect,
       setNumberFunc: setnum_collects,
       article: articleObj,
-    })
-  }
+    });
+  };
 
   const handleLike = async () => {
     await handleAction({
-      actionName: '点赞',
-      actionName_en: 'likes',
+      actionName: "点赞",
+      actionName_en: "likes",
       myItem: myLike,
       createFunc: createLike,
       delectMyItemFunc: deleteLike,
@@ -136,8 +157,8 @@ function MyComponent() {
       setMyItemFunc: setmyLike,
       setNumberFunc: setnum_likes,
       article: articleObj,
-    })
-  }
+    });
+  };
 
   const handleAction = async ({
     actionName,
@@ -152,42 +173,49 @@ function MyComponent() {
     article,
   }) => {
     if (myItem) {
-      await delectMyItemFunc({ item: myItem })
-      setNumberFunc(await countAllFuc({ article }))
-      setMyItemFunc(await getMyItemFunc({ article }))
-      message.success(`取消${actionName}`)
+      await delectMyItemFunc({ item: myItem });
+      setNumberFunc(await countAllFuc({ article }));
+      setMyItemFunc(await getMyItemFunc({ article }));
+      message.success(`取消${actionName}`);
       // 更新article 相关数量字段
       updateArticle({
         articleItem: article,
-        params: { [`${actionName_en}`]: -1 }
-      })
+        params: { [`${actionName_en}`]: -1 },
+      });
     } else {
-      await createFunc({ article })
-      setNumberFunc(await countAllFuc({ article }))
-      setMyItemFunc(await getMyItemFunc({ article }))
-      message.success(`${actionName}成功`)
+      await createFunc({ article });
+      setNumberFunc(await countAllFuc({ article }));
+      setMyItemFunc(await getMyItemFunc({ article }));
+      message.success(`${actionName}成功`);
       // 更新article 相关数量字段
       updateArticle({
         articleItem: article,
-        params: { [`${actionName_en}`]: 1 }
-      })
+        params: { [`${actionName_en}`]: 1 },
+      });
     }
-  }
+  };
 
   return (
     <Layout
       onChange={(params) => {
-        setprofile(params.profile)
-        setuserinfo(params.userinfo)
+        setprofile(params.profile);
+        setuserinfo(params.userinfo);
       }}
     >
       <BackTop />
       {/* 文章列表 */}
-      {articleObj && articleObjJSON && (
+      {articleObj && articleObjJSON && profile && (
         <div className={styles.container}>
+          <Head>
+            <title>{`${articleObjJSON.title} ${profile.title}`}</title>
+          </Head>
           <div className={styles.left}>
             <div className={styles.left_content}>
-              <UserInfo userinfo={author} time={dayjs(articleObj.createdAt).format('YYYY/MM/DD')} views={views} />
+              <UserInfo
+                userinfo={author}
+                time={dayjs(articleObj.createdAt).format("YYYY/MM/DD")}
+                views={views}
+              />
               <p className={styles.title}>{articleObjJSON.title}</p>
               <article
                 className="markdown-body"
@@ -195,47 +223,18 @@ function MyComponent() {
                   __html: html,
                 }}
               ></article>
-              <div className={styles.actions}>
-                <div className={myLike ? styles.actions_item_active : styles.actions_item} onClick={handleLike}>
-                  <i className="iconfont icon-tubiaozhizuo-"></i>
-                  {num_likes ? <span className={styles.number}>{num_likes}</span> : ''}
-                </div>
-                {/* <div className={styles.actions_item}>
-                  <i className="iconfont icon-tubiaozhizuo--copy"></i>
-                  {articleObjJSON.dislikes && <span className={styles.number}>{articleObjJSON.dislikes || 0}</span>}
-                </div> */}
-                <div className={myCollect ? styles.actions_item_active : styles.actions_item} onClick={handleCollect}>
-                  <i className="iconfont icon-star"></i>
-                  {num_collects ? <span className={styles.number}>{num_collects}</span> : ''}
-                </div>
-                <ReactScroll.Link spy={true} smooth={true} to="article-comments">
-                  <div className={styles.actions_item}>
-                    <i className="iconfont icon-message_three_points"></i>
-                    {num_comments && <span className={styles.number}>{num_comments}</span>}
-                  </div>
-                </ReactScroll.Link>
-              </div>
             </div>
             <div className={styles.left_content} id="article-comments">
               <Comments type="article" id={articleObj} userinfo={userinfo} />
             </div>
           </div>
           <div className={styles.right}>
-            {/* {author && (
-              <div className={styles.author}>
-                <div className={styles.author_title}>关于作者</div>
-                <div className={styles.author_content}>
-                  <UserInfo userinfo={userinfo} />
-                </div>
-              </div>
-            )} */}
-            {profile && profile.ads && <Ad item={profile.ads[2]} />}
             <CategoryList nav={nav} />
           </div>
         </div>
       )}
     </Layout>
-  )
+  );
 }
 
-export default MyComponent
+export default MyComponent;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,14 +1,88 @@
 import Head from "next/head";
 import AV from "leancloud-storage";
 import dynamic from "next/dynamic";
+import { Tree, Button, notification, Input, Modal } from "antd";
+import React, { useState, useEffect } from "react";
 
 import styles from "./index.module.scss";
 import Layout from "src/components/www/Layout";
+import ArticleItem from "src/components/www/ArticleItem";
+import NoData from "src/components/www/NoData";
+import { getArticleList } from "src/service/article";
 
 function MyComponent() {
+  const [menus, setmenus] = useState(null);
+  const [articleLists, setarticleLists] = useState(null);
+
+  const getlist = async (params = {}) => {
+    const resList = await getArticleList({ status: 3, ...params });
+    setarticleLists(resList);
+  };
+
+  const onSelect = async (select, info) => {
+    const obj = {
+      title: info.node.title,
+      key: info.node.key,
+    };
+    let category_1_key = null;
+    let category_2_key = null;
+    menus.value.map((cate1Obj) => {
+      if (cate1Obj.key === obj.key) {
+        category_1_key = obj.key;
+      }
+      if (cate1Obj.children) {
+        cate1Obj.children.map((cate2Obj) => {
+          if (cate2Obj.key === obj.key) {
+            category_1_key = cate1Obj.key;
+            category_2_key = obj.key;
+          }
+        });
+      }
+    });
+    await getlist(
+      Object.assign(
+        { category_1_key },
+        category_2_key ? { category_2_key } : {}
+      )
+    );
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      await getlist();
+    }
+    fetchData();
+  }, []);
+
   return (
-    <Layout>
-      <div className={styles.container}>home</div>
+    <Layout
+      onChange={(params) => {
+        console.log(params.menus);
+        setmenus(params.menus);
+      }}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.list}>
+            {articleLists &&
+              articleLists.length > 0 &&
+              articleLists.map((obj) => {
+                return <ArticleItem key={obj.id} data={obj.toJSON()} />;
+              })}
+            {articleLists && articleLists.length === 0 && <NoData />}
+          </div>
+        </div>
+        <div className={styles.category}>
+          {menus && (
+            <Tree
+              showLine
+              defaultExpandAll
+              treeData={menus.value}
+              onSelect={onSelect}
+            />
+          )}
+        </div>
+      </div>
     </Layout>
   );
 }
